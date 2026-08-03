001779fe504bf881fedeae37ffe662e3dcc82ef5
414-12-karnpapha <54275@yupparaj.ac.th>
2026-08-03 14:23:56 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -5,3 +5,5 @@ vat = price * 0.07
 net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.divider()
+st.write("นางสาวกานต์ปภา เฮ้าวัน เลขที่ 12  ม.4/14")
